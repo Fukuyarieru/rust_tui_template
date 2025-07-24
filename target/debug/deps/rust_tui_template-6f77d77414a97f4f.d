e0abd73c69b103cd8d6e662a1a155280fd0470d2
PzzaZZa <arick.kfn@gmail.com>
2025-07-24 09:13:46 +0000

    Jul 24, 2025, 12:13 PM

---
+++ target/debug/deps/rust_tui_template-6f77d77414a97f4f.d
@@ -0,0 +1,18 @@
+E:\rust_tui_template\target\debug\deps\rust_tui_template-6f77d77414a97f4f.d: src\main.rs src\prelude.rs src\standard\mod.rs src\standard\grid.rs src\standard\prelude.rs src\standard\color.rs src\tui\mod.rs src\tui\display_object.rs src\tui\terminal.rs src\tui\default_regions.rs src\tui\display.rs src\tui\pixel.rs src\tui\text_modifers.rs src\tui\prelude.rs
+
+E:\rust_tui_template\target\debug\deps\librust_tui_template-6f77d77414a97f4f.rmeta: src\main.rs src\prelude.rs src\standard\mod.rs src\standard\grid.rs src\standard\prelude.rs src\standard\color.rs src\tui\mod.rs src\tui\display_object.rs src\tui\terminal.rs src\tui\default_regions.rs src\tui\display.rs src\tui\pixel.rs src\tui\text_modifers.rs src\tui\prelude.rs
+
+src\main.rs:
+src\prelude.rs:
+src\standard\mod.rs:
+src\standard\grid.rs:
+src\standard\prelude.rs:
+src\standard\color.rs:
+src\tui\mod.rs:
+src\tui\display_object.rs:
+src\tui\terminal.rs:
+src\tui\default_regions.rs:
+src\tui\display.rs:
+src\tui\pixel.rs:
+src\tui\text_modifers.rs:
+src\tui\prelude.rs:
